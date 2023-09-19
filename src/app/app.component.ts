6bf5c96fb3fad271385f39ccfdb174bae808277c
Marci1901 <marcelloluigi.rizzi@gmail.com>
2023-09-19 11:40:33 +0200

    Kick starter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
-  templateUrl: 'app.component.html',
+  templateUrl:'app.component.html',
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  public appPages = [];
+  public appPages = [
+    { title:'', url:'', icon: ''}
+  ];
+  public labels =[];
   constructor() {}
 }
